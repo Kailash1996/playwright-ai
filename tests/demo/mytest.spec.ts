edb48b08a867dc4f65905ed4c62e25ead40bcbf6
Kailash1996 <67107630+Kailash1996@users.noreply.github.com>
2026-05-05 20:24:22 +0530

    new data tests

--- tests/demo/mytest.spec.ts
+++ tests/demo/mytest.spec.ts
@@ -1,5 +1,5 @@
 import { test, expect } from "@playwright/test";
-
+import constants from "../../data/constants.json"
 test("new Title assertion", async ({ page }) => {
   await page.goto("https://katalon-demo-cura.herokuapp.com/");
   await expect(page).toHaveTitle("CURA Healthcare Service");
@@ -10,9 +10,13 @@ test("new Title assertion", async ({ page }) => {
 //  //steps..
 // })
 
-test.only("locator demo", async ({ page }, testinfo) => {
+test("locator demo", async ({ page }, testinfo) => {
   await page.goto("https://katalon-demo-cura.herokuapp.com/");
   let makeappbtn =  page.getByRole("link", { name: "Make Appointment" })
   await makeappbtn.click();
   await expect(page.getByText("Please login to make")).toBeVisible();
+});
+
+test.only("static data", async ({ page }, testinfo) => {
+ console.log(`>> const data: ${JSON.stringify(constants.STATUSCODES)}`)
 });
